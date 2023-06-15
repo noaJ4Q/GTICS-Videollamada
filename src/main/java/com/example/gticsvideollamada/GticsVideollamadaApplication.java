b49b0464e1175eef94186e7e4f89839c29222b11
noe <noe.jaraq@gmail.com>
2023-06-14 23:50:12 -0500

    primer commit

---
+++ src/main/java/com/example/gticsvideollamada/GticsVideollamadaApplication.java
@@ -0,0 +1,13 @@
+package com.example.gticsvideollamada;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GticsVideollamadaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GticsVideollamadaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/gticsvideollamada/GticsVideollamadaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gticsvideollamada;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GticsVideollamadaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
